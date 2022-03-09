027e2024afc1e1142af4d657ba22b6f969a90262
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-09 00:52:51 -0600

    lol

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './index.css';
 import './Components/Layout';
-import Layout from './Components/Layout';
-
 function App() {
   return (
-    <Layout/>
     <div className="bg-blue-300 h-screen">
-     IKITI INICIO 2
+     IKITI INICIO
     </div>
   );
 }
