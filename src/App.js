596da8a44a9b6ac56c8a1bb143f14a953f70a5e8
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-09 03:15:09 -0600

    menu 02

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import ResponsiveNavbar from './ResponsiveNavbar';
 import Navbar from './Components/Navbar';
-
+import Example from './Components/Navbar2';
 
 function App() {
   return (
     <div className="App w-full h-screen">
       <Navbar />
+      <Example />
     </div>
   );
 }
