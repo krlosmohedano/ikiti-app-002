d455c99a326a7d73fc33ace3bcc9022575dc7e3e
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-08 23:37:33 -0600

    tailwind backgorund

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './index.css';
 function App() {
   return (
-    <div className="bg-blue-300">
+    <div className="bg-blue-300 h-screen">
      IKITI
     </div>
   );
