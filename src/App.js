4741c002aa78728777729d2435833e5a6c91cf14
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-09 01:02:46 -0600

    menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import './index.css';
 import React from "react";
 
 export default function Menu({ fixed }) {
@@ -14,7 +15,7 @@ export default function Menu({ fixed }) {
                   className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                   href="#pablo"
                 >
-                  lightBlue Starter Menu
+                  IKITI STORE
                 </a>
                 <button
                   className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
