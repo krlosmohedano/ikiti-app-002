7f823e0e6133f13e8497a1235949cb1ceb13326c
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-09 02:48:01 -0600

    menuuu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import ResponsiveNavbar from './ResponsiveNavbar';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <div className="App w-full h-screen">
       <ResponsiveNavbar />
+      <Navbar />
     </div>
   );
 }
