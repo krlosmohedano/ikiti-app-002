f3f389c3ff7b8772d19f39fe2fe5123c05a23a95
Carlos Mohedano <krlosmohedano@gmail.com>
2022-03-09 02:26:59 -0600

    menu 034

--- src/ResponsiveNavbar.js
+++ src/ResponsiveNavbar.js
@@ -12,7 +12,6 @@ export default function ResponsiveNavbar() {
     }
 
     return (
-        import React from "react";
 
         export default function Navbar({ fixed }) {
           const [navbarOpen, setNavbarOpen] = React.useState(false);
